07050ebc3abb94470fbae7c948a61ab0aea173e6
Jussi Viinikainen <jussi_viinikainen@hotmail.com>
2023-07-11 19:49:43 -0500

    Update fluctuation reduction in track pair efficiency correction

--- src/TrackPairEfficiencyCorrector.cxx
+++ src/TrackPairEfficiencyCorrector.cxx
@@ -72,15 +72,15 @@ void TrackPairEfficiencyCorrector::ReadCorrectionTables(){
   fJetPtBinBorders[fnJetPtBins] = fCard->GetHighBinBorderJetPt(fnJetPtBins-1);
   
   // Select the correct name for the corrections
-  const char* histogramName = fUseSmoothedCorrection ? "smoothedTrackPairEfficiencyCorrection" : "trackPairEfficiencyCorrection";  
+  const char* smoothString = fUseSmoothedCorrection ? "smoothedT" : "t";
 
   // Read all the tables from the file
   for(int iCentrality = 0; iCentrality < fnCentralityBins; iCentrality++){
     for(int iTriggerPt = 0; iTriggerPt < fnTrackPtBins; iTriggerPt++){
       for(int iAssociatedPt = 0; iAssociatedPt <= iTriggerPt; iAssociatedPt++){
-        fCorrectionTable[iCentrality][iTriggerPt][iAssociatedPt] = (TH1D*) fInputFile->Get(Form("%s_C%dT%dA%d", histogramName, iCentrality, iTriggerPt, iAssociatedPt));
+        fCorrectionTable[iCentrality][iTriggerPt][iAssociatedPt] = (TH1D*) fInputFile->Get(Form("%srackPairEfficiencyCorrection_C%dT%dA%d", smoothString, iCentrality, iTriggerPt, iAssociatedPt));
         for(int iJetPt = 0; iJetPt < fnJetPtBins; iJetPt++){
-          fCorrectionTableCloseToJet[iCentrality][iTriggerPt][iAssociatedPt][iJetPt] = (TH1D*) fInputFile->Get(Form("trackPairEfficiencyCorrectionCloseToJet_C%dT%dA%dJ%d", iCentrality, iTriggerPt, iAssociatedPt, iJetPt));
+          fCorrectionTableCloseToJet[iCentrality][iTriggerPt][iAssociatedPt][iJetPt] = (TH1D*) fInputFile->Get(Form("%srackPairEfficiencyCorrectionCloseToJet_C%dT%dA%dJ%d", smoothString, iCentrality, iTriggerPt, iAssociatedPt, iJetPt));
         }
       }
     }
@@ -141,45 +141,88 @@ std::pair<double,double> TrackPairEfficiencyCorrector::GetTrackPairEfficiencyCor
   
   if(fDisableCorrection) return std::make_pair(1,0);
 
+  // We do not need to correct anything above 0.4, since that is outside of the analysis range
+  if(deltaR > 0.4) return std::make_pair(1,0);
+
+  // Find the higher and lower pT from trigger and associated particles
+  double higherPt = triggerPt;
+  double lowerPt = associatedPt;
+  if(associatedPt > triggerPt){
+    higherPt = associatedPt;
+    lowerPt = triggerPt;
+  }
+
   // Determine the correct bin from the centrality and track pT values
   int iCentrality = FindCentralityBin(centrality);
-  int iTriggerPt = FindTrackPtBin(triggerPt);
-  int iAssociatedPt = FindTrackPtBin(associatedPt);
+  int iTriggerPt = FindTrackPtBin(higherPt);
+  int iAssociatedPt = FindTrackPtBin(lowerPt);
   double correction = 1;
   double error = 0;
+  bool skipJetCorrection = false;
 
   // If jet pT is given, use the correction with jet pT bins
-  if(jetPt > 0) { // There are no statistics for very high pT jets with very high pT tracks, so relax the jet requirement there
-    if(deltaR > 0.4) return std::make_pair(1,0);
-    int iJetPt = FindJetPtBin(jetPt);
-    // Ensure that the trigger pT is higher than associated pT
-    if(iTriggerPt >= iAssociatedPt){
+  if(jetPt > 0) {
+
+    // For pp, in some of the highest bins there is not enough statistics close to the jets. Use the correction without jets in these cases
+    if(!fUseSmoothedCorrection){
+      if(higherPt > 50 && lowerPt > 20){
+        skipJetCorrection = true;
+      } else if (higherPt > 100){
+        skipJetCorrection = true;
+      }
+    }
+
+    // For PbPb, in some of the highest bins there is not enough statistics close to the jets. Use the correction without jets in these cases
+    if(fUseSmoothedCorrection){
+      if(higherPt > 50 && lowerPt > 12){
+        skipJetCorrection = true;
+      } else if (higherPt > 20 && lowerPt > 20){
+        skipJetCorrection = true;
+      }
+    }
+
+    // If we are in bins where the statistics is not a problem, do the correction in jet pT bins
+    if(!skipJetCorrection){
+
+      // For small deltaR, proceed with the correction
+      int iJetPt = FindJetPtBin(jetPt);
+
       correction = 1.0/fCorrectionTableCloseToJet[iCentrality][iTriggerPt][iAssociatedPt][iJetPt]->GetBinContent(fCorrectionTableCloseToJet[iCentrality][iTriggerPt][iAssociatedPt][iJetPt]->FindBin(deltaR));
       error = fCorrectionTableCloseToJet[iCentrality][iTriggerPt][iAssociatedPt][iJetPt]->GetBinError(fCorrectionTableCloseToJet[iCentrality][iTriggerPt][iAssociatedPt][iJetPt]->FindBin(deltaR));
-    } else {
-      correction = 1.0/fCorrectionTableCloseToJet[iCentrality][iAssociatedPt][iTriggerPt][iJetPt]->GetBinContent(fCorrectionTableCloseToJet[iCentrality][iAssociatedPt][iTriggerPt][iJetPt]->FindBin(deltaR));
-      error = fCorrectionTableCloseToJet[iCentrality][iAssociatedPt][iTriggerPt][iJetPt]->GetBinError(fCorrectionTableCloseToJet[iCentrality][iAssociatedPt][iTriggerPt][iJetPt]->FindBin(deltaR));
-    }
 
-    // Check that the correction is sane
-    if(1.0/correction > 0.01 && 1.0/correction < 2){
-      return std::make_pair(correction,error);
-    }
+      // Check that the correction is sane
+      if(1.0/correction > 0.01 && 1.0/correction < 2){
+        return std::make_pair(correction,error);
+      }
 
-    // If the correction does not make sense, use the correction without jet pT bins instead
-    std::cout << "Insane track pair efficiency correction in bin: triggerPt: " << iTriggerPt << " assoc pT: " << iAssociatedPt << " jetPt: " << iJetPt  << " deltaR: " << deltaR << std::endl;
-    std::cout << "Using the correction without jets instead" << std::endl;
+      // If the correction does not make sense, use the correction without jet pT bins instead
+      std::cout << "Insane track pair efficiency correction in bin: triggerPt: " << iTriggerPt << " assoc pT: " << iAssociatedPt << " jetPt: " << iJetPt  << " deltaR: " << deltaR << std::endl;
+      std::cout << "Using the correction without jets instead" << std::endl;
+
+    } // If for not skipping the corrections
     
+  } // If for correction with jet pT bins
+
+  // For pp, in some of the highest bins we skip the correction since we only see fluctuations there
+  if(!fUseSmoothedCorrection){
+    if(higherPt > 100 && lowerPt > 50){
+      if(deltaR > 0.02) return std::make_pair(1,0);
+    }
+  }
+
+  // Also for PbPb, we do not apply the correction in the highest bins due to not having enough statistics
+  if(fUseSmoothedCorrection){
+    if(higherPt > 50){
+      if(lowerPt > 50 && deltaR > 0.028) return std::make_pair(1,0);
+      if(lowerPt > 20 && deltaR > 0.05) return std::make_pair(1,0);
+      if(lowerPt > 16 && deltaR > 0.1) return std::make_pair(1,0);
+    }
   }
     
   // Ensure that the trigger pT is higher than associated pT
-  if(iTriggerPt >= iAssociatedPt){
-    correction = 1.0/fCorrectionTable[iCentrality][iTriggerPt][iAssociatedPt]->GetBinContent(fCorrectionTable[iCentrality][iTriggerPt][iAssociatedPt]->FindBin(deltaR));
-    error = fCorrectionTable[iCentrality][iTriggerPt][iAssociatedPt]->GetBinError(fCorrectionTable[iCentrality][iTriggerPt][iAssociatedPt]->FindBin(deltaR));
-  } else {
-    correction = 1.0/fCorrectionTable[iCentrality][iAssociatedPt][iTriggerPt]->GetBinContent(fCorrectionTable[iCentrality][iAssociatedPt][iTriggerPt]->FindBin(deltaR));
-    error = fCorrectionTable[iCentrality][iAssociatedPt][iTriggerPt]->GetBinError(fCorrectionTable[iCentrality][iAssociatedPt][iTriggerPt]->FindBin(deltaR));
-  }
+  correction = 1.0/fCorrectionTable[iCentrality][iTriggerPt][iAssociatedPt]->GetBinContent(fCorrectionTable[iCentrality][iTriggerPt][iAssociatedPt]->FindBin(deltaR));
+  error = fCorrectionTable[iCentrality][iTriggerPt][iAssociatedPt]->GetBinError(fCorrectionTable[iCentrality][iTriggerPt][iAssociatedPt]->FindBin(deltaR));
+ 
   return std::make_pair(correction, error);
 }
 
