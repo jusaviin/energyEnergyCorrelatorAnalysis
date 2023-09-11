c769604b9d6bbb7da4411fcf04e207bfffbe4ed1
Jussi Viinikainen <jussi_viinikainen@hotmail.com>
2023-09-11 17:28:10 -0500

    Added macro to compare statistical errors to systematic ones

---
+++ plotting/statisticalToSystematicErrorRatioPrinter.C
@@ -0,0 +1,204 @@
+#include "SystematicUncertaintyOrganizer.h" R__LOAD_LIBRARY(plotting/DrawingClasses.so)
+#include "EECHistogramManager.h"
+#include "EECCard.h"
+
+/*
+ * Macro for making final result plots comparing energy-energy correlators between pp and PbPb
+ */
+void statisticalToSystematicErrorRatioPrinter(){
+
+  enum enumDataType{kPbPb, kPp, kNDataTypes};
+
+  // ============= //
+  // Configuration //
+  // ============= //
+  
+  // Input files
+  TString inputFileName[kNDataTypes];
+  inputFileName[kPbPb] = "data/eecAnalysis_akFlowJet_wtaAxis_newTrackPairEfficiencySmoothed_unfoldingWithNominalSmear_processed_2023-07-13.root";
+  inputFileName[kPp] = "data/ppData_pfJets_wtaAxis_newTrackPairEfficiency_unfoldingWithNominalSmear_processed_2023-07-13.root";
+  TString uncertaintyFileName[kNDataTypes];
+  uncertaintyFileName[kPbPb] = "systematicUncertainties/systematicUncertainties_jetMetUpdate_includeMCnonClosure_2023-07-16.root";
+  uncertaintyFileName[kPp] = "systematicUncertainties/systematicUncertaintiesForPp_jetMetUpdate_includeMCnonClosure_2023-07-16.root";
+  
+  TFile* inputFile[kNDataTypes];
+  TFile* uncertaintyFile[kNDataTypes];
+  EECCard* card[kNDataTypes];
+  EECCard* uncertaintyCard[kNDataTypes];
+  for(int iFile = 0; iFile < kNDataTypes; iFile++){
+
+    // Load the input file
+    inputFile[iFile] = TFile::Open(inputFileName[iFile]);
+
+    // Check that the input file exists
+    if(inputFile[iFile] == NULL){
+      cout << "Error! The file " << inputFileName[iFile].Data() << " does not exist!" << endl;
+      cout << "Maybe you forgot the data/ folder path?" << endl;
+      cout << "Will not execute the code" << endl;
+      return;
+    }
+    
+    // Load the card from the file and read the collision system
+    card[iFile] = new EECCard(inputFile[iFile]);
+
+    // Load the uncertainty file
+    uncertaintyFile[iFile] = TFile::Open(uncertaintyFileName[iFile]);
+
+    // Check that the uncertianty file exists
+    if(uncertaintyFile[iFile] == NULL){
+      cout << "Error! The file " << uncertaintyFileName[iFile].Data() << " does not exist!" << endl;
+      cout << "Maybe you forgot the systematicUncertainties/ folder path?" << endl;
+      cout << "Will not execute the code" << endl;
+      return;
+    }
+    
+    // Load the card from the file and read the collision system
+    uncertaintyCard[iFile] = new EECCard(uncertaintyFile[iFile]);
+  }
+
+  // ==================================================================
+  // ========================= Configuration ==========================
+  // ==================================================================
+  
+  // Find the number of bins from the PbPb card
+  const int nCentralityBins = card[kPbPb]->GetNCentralityBins();
+  const int nJetPtBinsEEC = card[kPbPb]->GetNJetPtBinsEEC();
+  const int nTrackPtBinsEEC = card[kPbPb]->GetNTrackPtBinsEEC();
+  
+  // The final results are available for all the bins that are unfolded
+  int firstDrawnCentralityBin = card[kPbPb]->GetFirstUnfoldedCentralityBin();
+  int lastDrawnCentralityBin = card[kPbPb]->GetLastUnfoldedCentralityBin();
+  
+  int firstDrawnJetPtBinEEC = card[kPbPb]->GetFirstUnfoldedJetPtBin();
+  int lastDrawnJetPtBinEEC = card[kPbPb]->GetLastUnfoldedJetPtBin();
+  
+  int firstDrawnTrackPtBinEEC = card[kPbPb]->GetFirstUnfoldedTrackPtBin();
+  int lastDrawnTrackPtBinEEC = card[kPbPb]->GetLastUnfoldedTrackPtBin();
+
+  // Range from which ratio is calculated
+  std::pair<double, double> analysisDeltaR = std::make_pair(0.008, 0.39); // DeltaR span in which the analysis is done
+
+  // =============================================== //
+  // Read the histograms from the histogram managers //
+  // =============================================== //
+
+  // Create histogram managers for result files and systematic uncertainty organizers for systematic uncertainty files
+  EECHistogramManager* histograms[kNDataTypes];
+  SystematicUncertaintyOrganizer* uncertainties[kNDataTypes];
+  
+  for(int iDataType = 0; iDataType < kNDataTypes; iDataType++){
+
+    // Create a new histogram manager
+    histograms[iDataType] = new EECHistogramManager(inputFile[iDataType], card[iDataType]);
+  
+    // Load all unfolded energy-energy correlators
+    histograms[iDataType]->SetLoadEnergyEnergyCorrelators(true);
+    histograms[iDataType]->SetCentralityBinRange(card[iDataType]->GetFirstUnfoldedCentralityBin(), card[iDataType]->GetLastUnfoldedCentralityBin());
+    histograms[iDataType]->SetTrackPtBinRangeEEC(card[iDataType]->GetFirstUnfoldedTrackPtBin(), card[iDataType]->GetLastUnfoldedTrackPtBin());
+    histograms[iDataType]->SetJetPtBinRangeEEC(card[iDataType]->GetFirstUnfoldedJetPtBin(), card[iDataType]->GetLastUnfoldedJetPtBin());
+
+    // Load the histograms from the file
+    histograms[iDataType]->LoadProcessedHistograms();
+
+    // Create a new systematic uncertainty organizer
+    uncertainties[iDataType] = new SystematicUncertaintyOrganizer(uncertaintyFile[iDataType]);
+
+  }
+ 
+  // Energy-energy correlators and PbPb to pp ratios
+  TH1D* energyEnergyCorrelatorSignalPbPb[nCentralityBins][nJetPtBinsEEC][nTrackPtBinsEEC];
+  TH1D* energyEnergyCorrelatorSignalPp[nJetPtBinsEEC][nTrackPtBinsEEC];
+  TH1D* systematicUncertaintyForPbPb[nCentralityBins][nJetPtBinsEEC][nTrackPtBinsEEC];
+  TH1D* systematicUncertaintyForPp[nJetPtBinsEEC][nTrackPtBinsEEC];
+
+  // Initialize histograms to NULL
+  for(int iJetPt = 0; iJetPt < nJetPtBinsEEC; iJetPt++){
+    for(int iTrackPt = 0; iTrackPt < nTrackPtBinsEEC; iTrackPt++){
+      energyEnergyCorrelatorSignalPp[iJetPt][iTrackPt] = NULL;
+      systematicUncertaintyForPp[iJetPt][iTrackPt] = NULL;
+
+      for(int iCentrality = 0; iCentrality < nCentralityBins; iCentrality++){
+        energyEnergyCorrelatorSignalPbPb[iCentrality][iJetPt][iTrackPt] = NULL;
+        systematicUncertaintyForPbPb[iCentrality][iJetPt][iTrackPt] = NULL;
+      } // Centrality loop
+    } // Track pT loop
+  } // Jet pT loop
+
+  // Read the histograms from managers
+  int iTrackPtMatchedPp, iTrackPtMatchedPbPbUncertainty, iTrackPtMatchedPpUncertainty;
+  int iJetPtMatchedPp, iJetPtMatchedPbPbUncertainty, iJetPtMatchedPpUncertainty;
+  int iCentralityMatched;
+
+  for(int iJetPt = firstDrawnJetPtBinEEC; iJetPt <= lastDrawnJetPtBinEEC; iJetPt++){
+    iJetPtMatchedPp = card[kPp]->FindBinIndexJetPtEEC(card[kPbPb]->GetBinBordersJetPtEEC(iJetPt));
+    iJetPtMatchedPbPbUncertainty = uncertaintyCard[kPbPb]->FindBinIndexJetPtEEC(card[kPbPb]->GetBinBordersJetPtEEC(iJetPt));
+    iJetPtMatchedPpUncertainty = uncertaintyCard[kPp]->FindBinIndexJetPtEEC(card[kPbPb]->GetBinBordersJetPtEEC(iJetPt));
+    for(int iTrackPt = firstDrawnTrackPtBinEEC; iTrackPt <= lastDrawnTrackPtBinEEC; iTrackPt++){
+      iTrackPtMatchedPp = card[kPp]->FindBinIndexTrackPtEEC(card[kPbPb]->GetBinBordersTrackPtEEC(iTrackPt));
+      iTrackPtMatchedPbPbUncertainty = uncertaintyCard[kPbPb]->FindBinIndexTrackPtEEC(card[kPbPb]->GetBinBordersTrackPtEEC(iTrackPt));
+      iTrackPtMatchedPpUncertainty = uncertaintyCard[kPp]->FindBinIndexTrackPtEEC(card[kPbPb]->GetBinBordersTrackPtEEC(iTrackPt));
+
+      // Read the pp histograms that do not have centrality binning
+      energyEnergyCorrelatorSignalPp[iJetPt][iTrackPt] = histograms[kPp]->GetHistogramEnergyEnergyCorrelatorProcessed(EECHistogramManager::kEnergyEnergyCorrelator, 0, iJetPtMatchedPp, iTrackPtMatchedPp, EECHistogramManager::kEnergyEnergyCorrelatorUnfoldedSignal);
+      systematicUncertaintyForPp[iJetPt][iTrackPt] = uncertainties[kPp]->GetSystematicUncertainty(0, iJetPtMatchedPpUncertainty, iTrackPtMatchedPpUncertainty);
+
+      for(int iCentrality = firstDrawnCentralityBin; iCentrality <= lastDrawnCentralityBin; iCentrality++){
+        iCentralityMatched = uncertaintyCard[kPbPb]->FindBinIndexCentrality(card[kPbPb]->GetBinBordersCentrality(iCentrality));
+
+        // Read the PbPb histograms
+        energyEnergyCorrelatorSignalPbPb[iCentrality][iJetPt][iTrackPt] = histograms[kPbPb]->GetHistogramEnergyEnergyCorrelatorProcessed(EECHistogramManager::kEnergyEnergyCorrelator, iCentrality, iJetPt, iTrackPt, EECHistogramManager::kEnergyEnergyCorrelatorUnfoldedSignal);
+        systematicUncertaintyForPbPb[iCentrality][iJetPt][iTrackPt] = uncertainties[kPbPb]->GetSystematicUncertainty(iCentralityMatched, iJetPtMatchedPbPbUncertainty, iTrackPtMatchedPbPbUncertainty);
+      } // Centrality loop
+    } // Track pT loop
+  } // Jet pT loop
+
+  // ============================ //
+  //   Normalize the histograms   //
+  // ============================ //
+
+  double epsilon = 0.0001;
+  int lowAnalysisBin = energyEnergyCorrelatorSignalPbPb[firstDrawnCentralityBin][firstDrawnJetPtBinEEC][firstDrawnTrackPtBinEEC]->GetXaxis()->FindBin(analysisDeltaR.first + epsilon);
+  int highAnalysisBin = energyEnergyCorrelatorSignalPbPb[firstDrawnCentralityBin][firstDrawnJetPtBinEEC][firstDrawnTrackPtBinEEC]->GetXaxis()->FindBin(analysisDeltaR.second - epsilon);
+  for(int iJetPt = firstDrawnJetPtBinEEC; iJetPt <= lastDrawnJetPtBinEEC; iJetPt++){
+    for(int iTrackPt = firstDrawnTrackPtBinEEC; iTrackPt <= lastDrawnTrackPtBinEEC; iTrackPt++){
+      energyEnergyCorrelatorSignalPp[iJetPt][iTrackPt]->Scale(1.0 / energyEnergyCorrelatorSignalPp[iJetPt][iTrackPt]->Integral(lowAnalysisBin, highAnalysisBin, "width"));
+      systematicUncertaintyForPp[iJetPt][iTrackPt]->Scale(1.0 / systematicUncertaintyForPp[iJetPt][iTrackPt]->Integral(lowAnalysisBin, highAnalysisBin, "width"));
+      for(int iCentrality = firstDrawnCentralityBin; iCentrality <= lastDrawnCentralityBin; iCentrality++){
+        energyEnergyCorrelatorSignalPbPb[iCentrality][iJetPt][iTrackPt]->Scale(1.0 / energyEnergyCorrelatorSignalPbPb[iCentrality][iJetPt][iTrackPt]->Integral(lowAnalysisBin, highAnalysisBin, "width"));
+        systematicUncertaintyForPbPb[iCentrality][iJetPt][iTrackPt]->Scale(1.0 / systematicUncertaintyForPbPb[iCentrality][iJetPt][iTrackPt]->Integral(lowAnalysisBin, highAnalysisBin, "width"));
+      } // Centrality loop
+    } // Track pT loop
+  } // Jet pT loop
+
+  // ============================== //
+  //   Print the ratios to canvas   //
+  // ============================== //
+
+  double sumSystematicUncertainty;
+  double sumStatisticalUncertainty;
+
+  for(int iJetPt = firstDrawnJetPtBinEEC; iJetPt <= lastDrawnJetPtBinEEC; iJetPt++){
+    for(int iTrackPt = firstDrawnTrackPtBinEEC; iTrackPt <= lastDrawnTrackPtBinEEC; iTrackPt++){
+      sumSystematicUncertainty = 0;
+      sumStatisticalUncertainty = 0;
+      for(int iBin = lowAnalysisBin; iBin <= highAnalysisBin; iBin++){
+        sumStatisticalUncertainty += energyEnergyCorrelatorSignalPp[iJetPt][iTrackPt]->GetBinError(iBin);
+        sumSystematicUncertainty += systematicUncertaintyForPp[iJetPt][iTrackPt]->GetBinError(iBin);
+      }
+
+      cout << "Average syst/stat ratio in pp, iJetPt: " << iJetPt << " iTrackPt: " << iTrackPt << " is " << sumSystematicUncertainty/sumStatisticalUncertainty << endl;
+
+      for(int iCentrality = firstDrawnCentralityBin; iCentrality <= lastDrawnCentralityBin; iCentrality++){
+        sumSystematicUncertainty = 0;
+        sumStatisticalUncertainty = 0;
+        for(int iBin = lowAnalysisBin; iBin <= highAnalysisBin; iBin++){
+          sumStatisticalUncertainty += energyEnergyCorrelatorSignalPbPb[iCentrality][iJetPt][iTrackPt]->GetBinError(iBin);
+          sumSystematicUncertainty += systematicUncertaintyForPbPb[iCentrality][iJetPt][iTrackPt]->GetBinError(iBin);
+        }
+
+        cout << "Average syst/stat ratio in PbPb, iCent: " << iCentrality << " iJetPt: " << iJetPt << " iTrackPt: " << iTrackPt << " is " << sumSystematicUncertainty/sumStatisticalUncertainty << endl;
+
+      } // Centrality loop
+    } // Track pT loop
+  } // Jet pT loop
+}
