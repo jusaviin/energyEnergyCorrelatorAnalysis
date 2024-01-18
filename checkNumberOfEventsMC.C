b4fa76c9f604af96b4c112650b6418b0053360ad
Your Name <jussi.viinikainen@cern.ch>
2024-01-18 11:09:17 -0600

    Add PbPb data numbers to check

--- checkNumberOfEventsMC.C
+++ checkNumberOfEventsMC.C
@@ -3,7 +3,7 @@ void checkNumberOfEventsMC(const char* fileName){
   TH1D* vzHistogram = (TH1D*) dataFile->Get("vertexZ");
   int nEntries = vzHistogram->GetEntries();
 
-  int goodEntries[6] = {17764593, 17556674, 17764593+17556674, 21067938, 22279294, 21067938+22279294};
+  int goodEntries[6] = {17764593, 17556674, 17764593+17556674, 21067938, 22279294, 21067938+22279294, 11539863};
   
   cout << endl;
   cout << "Checking file " << fileName << endl;
@@ -19,6 +19,8 @@ void checkNumberOfEventsMC(const char* fileName){
     cout << "Yay! Entries consistent with part2 of Pythia8 simulation!" << endl;
   } else if (nEntries == goodEntries[5]){
     cout << "Yay! Entries consistent with whole Pythia8 simulation stats!" << endl;
+  } else if (nEntries == goodEntries[6]){
+    cout << "Yay! Entries consistent with PbPb data stats!" << endl;
   } else {
     cout << "ERROR! Entries do not match the expected!" << endl;
   }
