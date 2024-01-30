229fd289e464cca5585612775c5bab9edb4e0239
Your Name <jussi.viinikainen@cern.ch>
2024-01-29 19:48:50 -0600

    Use the latest uncertainty files

--- plotting/relativeUncertaintyPlotter.C
+++ plotting/relativeUncertaintyPlotter.C
@@ -34,12 +34,12 @@ void relativeUncertaintyPlotter(){
   
   // Input files
   TString uncertaintyFileName[kNDataTypes];
-  uncertaintyFileName[kPbPb] = "systematicUncertainties/systematicUncertainties_PbPb_nominalEnergyWeight_includeMCnonClosure_2024-01-22.root";
-  // systematicUncertainties_PbPb_nominalEnergyWeight_includeMCnonClosure_2024-01-22.root
-  // systematicUncertainties_PbPb_energyWeightSquared_includeMCnonClosure_2024-01-22.root
-  uncertaintyFileName[kPp] = "systematicUncertainties/systematicUncertainties_pp_nominalEnergyWeight_includeMCnonClosure_2024-01-22.root";
-  // systematicUncertainties_pp_nominalEnergyWeight_includeMCnonClosure_2024-01-22.root
-  // systematicUncertainties_pp_energyWeightSquared_includeMCnonClosure_2024-01-22.root
+  uncertaintyFileName[kPbPb] = "systematicUncertainties/systematicUncertainties_PbPb_energyWeightSquared_includeMCnonClosure_2024-01-29.root";
+  // systematicUncertainties_PbPb_nominalEnergyWeight_includeMCnonClosure_2024-01-29.root
+  // systematicUncertainties_PbPb_energyWeightSquared_includeMCnonClosure_2024-01-29.root
+  uncertaintyFileName[kPp] = "systematicUncertainties/systematicUncertainties_pp_energyWeightSquared_includeMCnonClosure_2024-01-29.root";
+  // systematicUncertainties_pp_nominalEnergyWeight_includeMCnonClosure_2024-01-29.root
+  // systematicUncertainties_pp_energyWeightSquared_includeMCnonClosure_2024-01-29.root
   TFile* uncertaintyFile[kNDataTypes];
   EECCard* uncertaintyCard[kNDataTypes];
   for(int iFile = 0; iFile < kNDataTypes; iFile++){
