580ee453881f757c251e4352e15edf1b964a4bf8
Your Name <jussi.viinikainen@cern.ch>
2024-02-06 15:51:57 -0600

    First version of signal to background scaling factor calculation

---
+++ plotting/findMeanPtDifferenceInResponseMatrix.C
@@ -0,0 +1,116 @@
+#include "EECHistogramManager.h" R__LOAD_LIBRARY(plotting/DrawingClasses.so)
+#include "EECCard.h"
+#include "JDrawer.h"
+#include "../src/EECHistograms.h"
+
+/*
+ * Macro for finding how much mean pT changes when unfolding with a response matrix
+ */
+void findMeanPtDifferenceInResponseMatrix(){
+  
+  // Input file
+  TString inputFileName = "data/PbPbMC2018_GenGen_akFlowJets_4pCentShift_cutBadPhi_optimizedUnfoldingBins_nominalSmear_oneDimensionalResponseMatrix_processed_2024-01-16.root";
+  // PbPbMC2018_GenGen_akFlowJets_4pCentShift_cutBadPhi_optimizedUnfoldingBins_nominalSmear_oneDimensionalResponseMatrix_processed_2024-01-16.root
+  // PbPbMC2018_GenGen_akFlowJets_4pCentShift_cutBadPhi_optimizedUnfoldingBins_energyWeightSquared_nominalSmear_oneDimensionalResponseMatrix_processed_2024-01-18.root
+
+  TFile* inputFile = TFile::Open(inputFileName);
+
+  // Check that the input file exists
+  if(inputFile == NULL){
+    cout << "Error! The file " << inputFileName.Data() << " does not exist!" << endl;
+    cout << "Maybe you forgot the data/ folder path?" << endl;
+    cout << "Will not execute the code" << endl;
+    return;
+  }
+
+  // Read EECCard from the file
+  EECCard* card = new EECCard(inputFile);
+  
+  // ====================================================
+  //               Binning configuration
+  // ====================================================
+  
+  // Find the number of bins from the card
+  const int nCentralityBins = card->GetNCentralityBins();
+  const int nJetPtBinsEEC = card->GetNJetPtBinsEEC();
+  
+  // Select explicitly which bins from the files are compared:
+  std::vector<std::pair<double,double>> comparedCentralityBin;
+  comparedCentralityBin.push_back(std::make_pair(4,14));
+  comparedCentralityBin.push_back(std::make_pair(14,34));
+  comparedCentralityBin.push_back(std::make_pair(34,54));
+  comparedCentralityBin.push_back(std::make_pair(54,94));
+
+  std::vector<std::pair<double,double>> comparedJetPtBin;
+  comparedJetPtBin.push_back(std::make_pair(120,140));
+  comparedJetPtBin.push_back(std::make_pair(140,160));
+  comparedJetPtBin.push_back(std::make_pair(160,180));
+  comparedJetPtBin.push_back(std::make_pair(180,200));
+  
+  // Create and setup a new histogram managers to project and handle the histograms
+  EECHistogramManager* histograms;
+  histograms = new EECHistogramManager(inputFile, card);
+
+  // Choose the energy-energy correlator types to load
+  histograms->SetLoadJetHistograms(true);
+  histograms->SetLoadJetPtOneDimensionalUnfoldingHistograms(true);
+
+  // Choose the bin ranges
+  histograms->SetCentralityBinRange(0, card->GetNCentralityBins() - 1);
+  histograms->SetJetPtBinRangeEEC(0, card->GetNJetPtBinsEEC() - 1);
+
+  // Load the histograms from the file
+  histograms->LoadProcessedHistograms();
+
+  // Energy-energy correlator histograms
+  TH1D* hJetPt[nCentralityBins];
+  TH2D* hSimpleResponseMatrix[nCentralityBins];
+  TProfile* hMeasuredMean[nCentralityBins];
+  
+  // Initialize the energy-energy correlator histogram arrays to NULL
+  for(int iCentrality = 0; iCentrality < nCentralityBins; iCentrality++){
+    hJetPt[iCentrality] = NULL;
+    hSimpleResponseMatrix[iCentrality] = NULL;
+    hMeasuredMean[iCentrality] = NULL;
+  } // Centrality loop
+  
+  // Helper variables
+  double epsilon = 0.001;
+  int iCentrality;
+  int lowIntegralBin, highIntegralBin;
+  double trueMean, measuredMean;
+
+  // Get the histograms from the histogram manager and see how much the mean jet pT shifts with unfolding
+  for(auto centralityBin : comparedCentralityBin){
+    iCentrality = card->FindBinIndexCentrality(centralityBin);
+
+    // Load the jet pT histogram for this centrality range
+    hJetPt[iCentrality] = histograms->GetHistogramJetPt(iCentrality);
+
+    // Load the one dimensional jet pT response matrix
+    hSimpleResponseMatrix[iCentrality] = histograms->GetHistogramJetPtOneDimensionalUnfoldingResponse(iCentrality);
+
+    // Get the profile from the response matrix to easily determine measured mean pT values
+    hMeasuredMean[iCentrality] = hSimpleResponseMatrix[iCentrality]->ProfileX();
+
+    // Loop over the selected jet pT ranges to see how much the mean jet pT is expected to change
+    for(auto jetPtBin : comparedJetPtBin){
+
+      // Calculate the true mean jet pT in a selected bin. This is the mean after unfolding.
+      lowIntegralBin = hJetPt[iCentrality]->FindBin(jetPtBin.first + epsilon);
+      highIntegralBin = hJetPt[iCentrality]->FindBin(jetPtBin.second - epsilon);
+      hJetPt[iCentrality]->GetXaxis()->SetRange(lowIntegralBin, highIntegralBin);
+      trueMean = hJetPt[iCentrality]->GetMean();
+
+      // Calculate the measured mean jet pT in a selected bin. This is the mean before unfolding.
+      measuredMean = hMeasuredMean[iCentrality]->GetBinContent(hMeasuredMean[iCentrality]->FindBin(jetPtBin.first + epsilon));
+
+      // Print the information to the console:
+      cout << "Results for centrality bin " << centralityBin.first << "-" << centralityBin.second << "  and jet pT bin " << jetPtBin.first << "-" << jetPtBin.second << endl;
+      cout << "Measured mean: " << measuredMean << "    True mean: " << trueMean << "   Upshift: " << trueMean - measuredMean << endl;
+
+    } // Jet pT loop
+  } // Centrality loop
+
+
+}
---
+++ plotting/signalToBackgroundRatio.C
@@ -0,0 +1,301 @@
+#include "EECHistogramManager.h" R__LOAD_LIBRARY(plotting/DrawingClasses.so)
+#include "EECCard.h"
+#include "JDrawer.h"
+#include "../src/EECHistograms.h"
+
+/*
+ * Macro for finding energy-energy correlator signal to background ratio from data
+ */
+void signalToBackgroundRatio(){
+  
+  // Input file
+  TString inputFileName = "data/eecAnalysis_akFlowJet_nominalEnergyWeight_optimizedUnfoldingBins_fixedCovarianceMatrix_unfoldingWithCovariance_processed_2024-01-23.root";
+  // eecAnalysis_akFlowJet_energyWeightSquared_optimizedUnfoldingBins_fixedCovarianceMatrix_unfoldingWithCovariance_processed_2024-01-23.root
+  // eecAnalysis_akFlowJet_nominalEnergyWeight_optimizedUnfoldingBins_fixedCovarianceMatrix_unfoldingWithCovariance_processed_2024-01-23.root
+
+  TFile* inputFile = TFile::Open(inputFileName);
+
+  // Check that the input file exists
+  if(inputFile == NULL){
+    cout << "Error! The file " << inputFileName.Data() << " does not exist!" << endl;
+    cout << "Maybe you forgot the data/ folder path?" << endl;
+    cout << "Will not execute the code" << endl;
+    return;
+  }
+
+  // Read EECCard from the file
+  EECCard* card = new EECCard(inputFile);
+  
+  // ====================================================
+  //               Binning configuration
+  // ====================================================
+  
+  // Find the number of bins from the card
+  const int nCentralityBins = card->GetNCentralityBins();
+  const int nJetPtBinsEEC = card->GetNJetPtBinsEEC();
+  const int nTrackPtBinsEEC = card->GetNTrackPtBinsEEC();
+  
+  // Select explicitly which bins from the files are compared:
+  std::vector<std::pair<double,double>> comparedCentralityBin;
+  comparedCentralityBin.push_back(std::make_pair(0,10));
+  comparedCentralityBin.push_back(std::make_pair(10,30));
+  comparedCentralityBin.push_back(std::make_pair(30,50));
+  comparedCentralityBin.push_back(std::make_pair(50,90));
+  bool individualCentrality = true; // True = make different figure for each bin. False = plot all centrality bin to the same figure.
+
+  std::vector<std::pair<double,double>> comparedJetPtBin;
+  comparedJetPtBin.push_back(std::make_pair(100,120));
+  comparedJetPtBin.push_back(std::make_pair(120,140));
+  comparedJetPtBin.push_back(std::make_pair(140,160));
+  comparedJetPtBin.push_back(std::make_pair(160,180));
+  comparedJetPtBin.push_back(std::make_pair(180,200));
+  comparedJetPtBin.push_back(std::make_pair(200,220));
+  bool individualJetPt = true; // True = make different figure for each bin. False = plot all jet pT bin to the same figure.
+
+  std::vector<double> comparedTrackPtBin;
+  comparedTrackPtBin.push_back(1.0);
+  comparedTrackPtBin.push_back(1.5);
+  comparedTrackPtBin.push_back(2.0);
+  comparedTrackPtBin.push_back(2.5);
+  comparedTrackPtBin.push_back(3.0);
+  bool individualTrackPt = true; // True = make different figure for each bin. False = plot all track pT bin to the same figure.
+
+
+  // Only allow one variable for which all bins are plotted to the same figure
+  if(individualCentrality + individualJetPt + individualTrackPt < 2){
+    cout << "You are tring to plot too many bins to the same figure!" << endl;
+    cout << "This macro can only plot all the bins from one variable." << endl;
+    cout << "Please check your configuration!" << endl; 
+    return;
+  }
+  
+  // Create and setup a new histogram managers to project and handle the histograms
+  EECHistogramManager* histograms;
+  histograms = new EECHistogramManager(inputFile, card);
+
+  // Choose the energy-energy correlator types to load
+  histograms->SetLoadJetHistograms(true);
+  histograms->SetLoadEnergyEnergyCorrelators(true);
+
+  // Choose the bin ranges
+  histograms->SetCentralityBinRange(0, card->GetNCentralityBins() - 1);
+  histograms->SetJetPtBinRangeEEC(0, card->GetNJetPtBinsEEC() - 1);
+  histograms->SetTrackPtBinRangeEEC(0, card->GetNTrackPtBinsEEC() - 1);
+
+  // Load the histograms from the file
+  histograms->LoadProcessedHistograms();
+
+  // Energy-energy correlator histograms
+  TH1D* hJetPt[nCentralityBins];
+  TH1D* hEnergyEnergyCorrelator[nCentralityBins][nJetPtBinsEEC][nTrackPtBinsEEC];
+  TH1D* hReflectedCone[nCentralityBins][nJetPtBinsEEC][nTrackPtBinsEEC];
+  
+  // Initialize the energy-energy correlator histogram arrays to NULL
+  for(int iCentrality = 0; iCentrality < nCentralityBins; iCentrality++){
+    hJetPt[iCentrality] = NULL;
+    for(int iJetPt = 0; iJetPt < nJetPtBinsEEC; iJetPt++){
+      for(int iTrackPt = 0; iTrackPt < nTrackPtBinsEEC; iTrackPt++){
+        hEnergyEnergyCorrelator[iCentrality][iJetPt][iTrackPt] = NULL;
+        hReflectedCone[iCentrality][iJetPt][iTrackPt] = NULL;
+      } // Track pT loop
+    } // Jet pT loop
+  } // Centrality loop
+  
+  // Helper variables
+  double integrationRange = 0.39;
+  double epsilon = 0.001;
+  int lowIntegralBin, highIntegralBin;
+  double signalIntegral, backgroundIntegral;
+  double signalIntegralError, backgroundIntegralError;
+  int iCentrality;
+  int iTrackPt;
+  int iJetPt;
+  int iJetMeanPt = 0;
+  
+  // Axes for mean jet pT vs. signal/background ratio plots 
+  const int nAnalyzedJetPtBins = comparedJetPtBin.size();
+  double jetMeanPtAxis[nCentralityBins][nTrackPtBinsEEC][nAnalyzedJetPtBins];
+  double jetMeanPtAxisError[nCentralityBins][nTrackPtBinsEEC][nAnalyzedJetPtBins];
+  double signalToBackgroundRatioAxis[nCentralityBins][nTrackPtBinsEEC][nAnalyzedJetPtBins];
+  double signalToBackgroundRatioAxisError[nCentralityBins][nTrackPtBinsEEC][nAnalyzedJetPtBins];
+
+  // Get the histograms from the histogram manager and calculate signal to background ratios and mean jet pT:s
+  for(auto centralityBin : comparedCentralityBin){
+    iCentrality = card->FindBinIndexCentrality(centralityBin);
+
+    // Load the jet pT histogram for this centrality range
+    hJetPt[iCentrality] = histograms->GetHistogramJetPt(iCentrality);
+
+    for(auto trackPtBin : comparedTrackPtBin){
+      iTrackPt = card->GetBinIndexTrackPtEEC(trackPtBin);
+      iJetMeanPt = 0;
+      for(auto jetPtBin : comparedJetPtBin){
+        iJetPt = card->FindBinIndexJetPtEEC(jetPtBin);
+
+        // Load the energy-energy correlator and reflected cone histograms
+        hEnergyEnergyCorrelator[iCentrality][iJetPt][iTrackPt] = histograms->GetHistogramEnergyEnergyCorrelator(EECHistogramManager::kEnergyEnergyCorrelator, iCentrality, iJetPt, iTrackPt);
+        hReflectedCone[iCentrality][iJetPt][iTrackPt] = histograms->GetHistogramEnergyEnergyCorrelator(EECHistogramManager::kEnergyEnergyCorrelator, iCentrality, iJetPt, iTrackPt, EECHistograms::kSignalReflectedConePair);
+
+        // Calculate the signal and background integrals
+        lowIntegralBin = 1;
+        highIntegralBin = hEnergyEnergyCorrelator[iCentrality][iJetPt][iTrackPt]->GetXaxis()->FindBin(integrationRange);
+        signalIntegral = hEnergyEnergyCorrelator[iCentrality][iJetPt][iTrackPt]->IntegralAndError(lowIntegralBin, highIntegralBin, signalIntegralError, "width");
+        backgroundIntegral = hReflectedCone[iCentrality][iJetPt][iTrackPt]->IntegralAndError(lowIntegralBin, highIntegralBin, backgroundIntegralError, "width");
+       
+        // Add the signal to background ratio and it's error to an array
+        signalToBackgroundRatioAxis[iCentrality][iTrackPt][iJetMeanPt] = signalIntegral / backgroundIntegral;
+        signalToBackgroundRatioAxisError[iCentrality][iTrackPt][iJetMeanPt] = TMath::Sqrt(TMath::Power(signalIntegralError / backgroundIntegral, 2) + TMath::Power(signalIntegral * backgroundIntegralError / backgroundIntegral / backgroundIntegral, 2));
+
+        // Calculate the mean pT values in the studied bins and put them to an array
+        lowIntegralBin = hJetPt[iCentrality]->FindBin(jetPtBin.first + epsilon);
+        highIntegralBin = hJetPt[iCentrality]->FindBin(jetPtBin.second - epsilon);
+        hJetPt[iCentrality]->GetXaxis()->SetRange(lowIntegralBin, highIntegralBin);
+        jetMeanPtAxis[iCentrality][iTrackPt][iJetMeanPt] = hJetPt[iCentrality]->GetMean();
+        jetMeanPtAxisError[iCentrality][iTrackPt][iJetMeanPt] = hJetPt[iCentrality]->GetMeanError();
+        iJetMeanPt++;
+
+      } // Jet pT loop
+    } // Track pT loop
+  } // Centrality loop
+
+  // Fill the obtained information into TGraphErrors
+  TGraphErrors* gSignalToBackgroundRatio[nCentralityBins][nTrackPtBinsEEC];
+
+  // Initialize the signal to background graphs to NULL
+  for(int iCentrality = 0; iCentrality < nCentralityBins; iCentrality++){
+    for(int iTrackPt = 0; iTrackPt < nTrackPtBinsEEC; iTrackPt++){
+      gSignalToBackgroundRatio[iCentrality][iTrackPt] = NULL;
+    } // Track pT loop
+  } // Centrality loop
+
+  // Make new graphs for the selected bins
+  for(auto centralityBin : comparedCentralityBin){
+    iCentrality = card->FindBinIndexCentrality(centralityBin);
+    for(auto trackPtBin : comparedTrackPtBin){
+      iTrackPt = card->GetBinIndexTrackPtEEC(trackPtBin);
+
+      gSignalToBackgroundRatio[iCentrality][iTrackPt] = new TGraphErrors(nAnalyzedJetPtBins, jetMeanPtAxis[iCentrality][iTrackPt], signalToBackgroundRatioAxis[iCentrality][iTrackPt], jetMeanPtAxisError[iCentrality][iTrackPt], signalToBackgroundRatioAxisError[iCentrality][iTrackPt]);
+
+    } // Track pT loop
+  } // Centrality loop
+
+  // Create fit functions for signal to background ratio
+  TF1* fitToRatio[nCentralityBins][nTrackPtBinsEEC];
+
+  // Initialize the fit functions
+  for(int iCentrality = 0; iCentrality < nCentralityBins; iCentrality++){
+    for(int iTrackPt = 0; iTrackPt < nTrackPtBinsEEC; iTrackPt++){
+
+      if(iCentrality == 3){
+        fitToRatio[iCentrality][iTrackPt] = new TF1(Form("FitFunction%d%d", iCentrality, iTrackPt), "pol1", 120, 200);
+        fitToRatio[iCentrality][iTrackPt]->SetParameter(0,0);
+        fitToRatio[iCentrality][iTrackPt]->SetParameter(1,0.1);
+      } else {
+        fitToRatio[iCentrality][iTrackPt] = new TF1(Form("FitFunction%d%d", iCentrality, iTrackPt), "pol2", 120, 200);
+        fitToRatio[iCentrality][iTrackPt]->SetParameter(0,0);
+        fitToRatio[iCentrality][iTrackPt]->SetParameter(1,0.1);
+        fitToRatio[iCentrality][iTrackPt]->SetParameter(2,0);
+      }
+    }
+  }
+
+  // Draw the analyzed bins to a canvas
+  JDrawer* drawer = new JDrawer();
+  drawer->SetDefaultAppearanceGraph();
+
+  TLegend* legend;
+  TString centralityString, compactCentralityString;
+  TString trackPtString, compactTrackPtString;
+
+  double minimumValue, maximumValue, drawMargin;
+
+  // Draw each graph to separate canvas
+  for(auto centralityBin : comparedCentralityBin){
+    iCentrality = card->FindBinIndexCentrality(centralityBin);
+    centralityString = Form("Cent: %.0f-%.0f%%", centralityBin.first, centralityBin.second);
+    compactCentralityString = Form("_C=%.0f-%.0f", centralityBin.first, centralityBin.second);
+    for(auto trackPtBin : comparedTrackPtBin){
+      iTrackPt = card->GetBinIndexTrackPtEEC(trackPtBin);
+      trackPtString = Form("%.1f < track p_{T}", trackPtBin);
+      compactTrackPtString = Form("_T>%.1f", trackPtBin);
+      compactTrackPtString.ReplaceAll(".", "v");
+
+      // Setup the legend for plots
+      legend = new TLegend(0.18, 0.5, 0.58, 0.85);
+      legend->SetFillStyle(0); legend->SetBorderSize(0); legend->SetTextSize(0.05); legend->SetTextFont(62);
+      legend->AddEntry((TObject*)0, centralityString.Data(), "");
+      legend->AddEntry((TObject*)0, trackPtString.Data(), "");
+
+      // Find a good y-axis scale for the plots
+      minimumValue = gSignalToBackgroundRatio[iCentrality][iTrackPt]->GetPointY(0);
+      maximumValue = gSignalToBackgroundRatio[iCentrality][iTrackPt]->GetPointY(nAnalyzedJetPtBins-1);
+      drawMargin = (maximumValue - minimumValue) * 0.2;
+
+      // Set a fancy style for the graph
+      gSignalToBackgroundRatio[iCentrality][iTrackPt]->SetMarkerStyle(kFullCircle);
+      gSignalToBackgroundRatio[iCentrality][iTrackPt]->SetMarkerColor(kBlue);
+      gSignalToBackgroundRatio[iCentrality][iTrackPt]->SetLineColor(kBlue);
+
+      // Fit a line to the signal to background ratio
+      gSignalToBackgroundRatio[iCentrality][iTrackPt]->Fit(fitToRatio[iCentrality][iTrackPt]);
+
+      // Draw the graphs to canvas
+      drawer->DrawGraphCustomAxes(gSignalToBackgroundRatio[iCentrality][iTrackPt], comparedJetPtBin.at(0).first, comparedJetPtBin.at(nAnalyzedJetPtBins-1).second, minimumValue - drawMargin, maximumValue + drawMargin, "Jet p_{T}", "Signal / BG", " ", "a,p");
+
+      // Add the legend to the canvas
+      legend->Draw();
+      
+
+    } // Track pT loop
+  } // Centrality loop
+
+  // Print a table of the fit parameters to be copy-pasted into a corrector class
+    
+  cout << " double signalToBackgroundRatioFitParameters[" << comparedCentralityBin.size() << "][" << comparedTrackPtBin.size() << "][3] = {" << endl;
+    
+    for(auto centralityBin : comparedCentralityBin){
+      iCentrality = card->FindBinIndexCentrality(centralityBin);
+      
+      // Set the centrality information for legends and figure saving
+      cout << " // " << Form("Centrality %.0f-%.0f%%", centralityBin.first, centralityBin.second) << endl;
+      cout << "{";
+      
+      for(auto trackPtBin : comparedTrackPtBin){
+        iTrackPt = card->GetBinIndexTrackPtEEC(trackPtBin);
+        
+        cout << "{";
+        
+        for(int iParameter = 0; iParameter < 3; iParameter++){
+          
+          // Print the parameter value to the array:
+          if(iCentrality == 3 && iParameter == 2){
+            cout << 0;
+          } else {
+            cout << fitToRatio[iCentrality][iTrackPt]->GetParameter(iParameter);
+          }
+          
+          // Add the proper syntax
+          if(iParameter < 2){
+            cout << ",";
+          } else {
+            cout << "}";
+          }
+        } // Parameter pT loop
+        
+        if(trackPtBin != comparedTrackPtBin.back()){
+          cout << ", // " << Form("track pT > %.1f GeV", trackPtBin) << endl;
+        } else {
+          cout << "}";
+          if(trackPtBin != comparedTrackPtBin.back()){
+            cout << ", // " << Form("track pT > %.1f GeV", trackPtBin) << endl;
+          } else {
+            cout << " // " << Form("track pT > %.1f GeV", trackPtBin) << endl;
+          }
+        }
+        
+      } // Track pT loop
+    } // Centrality loop
+    cout << "};" << endl;
+
+
+}
